af25bbab9786e4416e8ec8f2a64b1dcd41d01b41
Stephan Bachmann <231126694+stephan-bachmann@users.noreply.github.com>
2025-10-15 11:21:21 +0900

    시그널 테스트 완료

--- main.asm
+++ main.asm
@@ -1,146 +1,20 @@
 default rel
 
-%include "defines.inc"
-%include "macros.inc"
-
-
-%macro SET_PIECE 1
-    mov byte [active_piece_state], %1
-    mov byte [active_piece_state+1], 0
-%endmacro
 
 
 global _start
-IMPORT clear
-IMPORT clear_set
-IMPORT cursor_visible
-IMPORT static_grid
-IMPORT color_grid
-IMPORT dynamic_grid
-extern active_piece_state
-extern set_grid
-extern update_dynamic_grid
-extern update_coordinate
-extern update_center_block_coordinate
-extern update_static_grid
-extern print_small_grid, print_static_grid
-extern rotate_piece
-extern sleep
-extern save_tty, restore_tty
-extern linefeed
-extern change_subgrid
-extern score
-extern set_score, add_score
-extern check_collision
-extern itoa
-extern fixing_piece
+extern set_timers
 
 section .text
 
 _start:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 8
-    ;call save_tty
+    call set_timers
 
-    call set_grid
-
-    mov rax, GRID_HEIGHT
-    dec rax
-    imul rax, GRID_WIDTH
-    mov rdx, RED
-.color_set:
-    mov byte [color_grid+rax], dl
-    inc rdx
-    inc rax
-    cmp rdx, RESET
-    jle .color_set
-    mov byte [color_grid+rax], BLACK
-    
-    mov rdi, 1000
-    call set_score
-
-    xor r14, r14
-.lll:
-    xor r12, r12
-.ll:
-    xor r13, r13
-    SET_PIECE r12b
-    xor rdi, rdi
-    mov rsi, r12
-    call change_subgrid
-    mov rdi, 1
-    mov rsi, r12
-    inc rsi
-    call change_subgrid
-    
 .l:
-    inc r14
-    PRNT clear
-    PRNT clear_set
-    mov rdi, 5
-    imul rdi, r14
-    mov rsi, 5
-    call update_center_block_coordinate
-    call update_coordinate
-    call check_collision
-
-    mov r15, rax
-
-    mov rdi, rax
-    lea rsi, qword [rbp-8]
-    call itoa
-
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, qword [rbp-8]
-    mov rdx, 1
-    syscall
-
-    mov rdi, 1
-    call linefeed
     
-    call update_dynamic_grid
-
-
-    mov rdi, dynamic_grid
-    mov rsi, GRID_WIDTH
-    mov rdx, GRID_HEIGHT
-    call print_small_grid
-
-    call fixing_piece
-    
-
-    mov rdi, 1
-    mov rsi, 0
-    call sleep
-
-    cmp r15, 1
-    je .next
-
-    call update_static_grid
-    call print_static_grid
-
-    call rotate_piece
-
-    mov rdi, -5
-    call add_score
-
-.next:
-    inc r13
-    cmp r13, 4
-    jne .l
-    
-    inc r12
-    cmp r12, 7
-    jne .ll
-
-    jmp .lll
-
+    jmp .l
 
 _exit:
-    PRNT cursor_visible
-    ;call restore_tty
 
     mov rax, 60
     xor rdi, rdi
---
+++ signal.asm
@@ -0,0 +1,190 @@
+default rel
+
+%define SYS_rt_sigaction  13
+%define SYS_rt_sigreturn  15
+%define SYS_timer_create  222
+%define SYS_timer_settime 223
+%define CLOCK_MONOTONIC   1
+%define SIGUSR1           10
+%define SIGUSR2           12
+%define SA_RESTART        0x10000000
+%define SA_RESTORER       0x04000000
+%macro PRNT 1
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, %1
+    mov rdx, %1_len
+    syscall
+%endmacro
+
+
+%macro LEN 1
+    %1_len equ $ - %1
+%endmacro
+
+section .data
+    sec_1: db "1sec", 0xa
+    LEN sec_1
+
+    sec_0_1: db "0.1sec", 0xa
+    LEN sec_0_1
+    txt1: db "1 installed", 0xa
+    txt1_len equ $ - txt1
+    failed1: db "1 failed", 0xa
+    failed1_len equ $ - failed1
+    txt2: db "2 installed", 0xa
+    txt2_len equ $ - txt2
+    failed2: db "2 failed", 0xa
+    failed2_len equ $ - failed2
+    global second_1_ticks, second_0_1_ticks
+    second_1_ticks:     dq 0 ; 64비트 정수
+    second_0_1_ticks:   dq 0 ; 64비트 정수
+
+
+    sa_usr1:
+        dq usr1_handler             ; 핸들러
+        dq SA_RESTART | SA_RESTORER ; 플래그
+        dq restorer                 ; 복귀자
+        times 16 dq 0               ; 마스크
+
+    sa_usr2:
+        dq usr2_handler             ; 핸들러
+        dq SA_RESTART | SA_RESTORER ; 플래그
+        dq restorer                 ; 복귀자
+        times 16 dq 0               ; 마스크
+
+
+    sevp_usr1:
+        dq 0                    ; sigev_value (미사용이면 0)
+        dd SIGUSR1              ; 시그널 번호
+        dd 0                    ; 타이머 만료 알림 유형(0 = 시그널)
+        times 12 dd 0           ; 패딩
+
+    sevp_usr2:
+        dq 0
+        dd SIGUSR2
+        dd 0
+        times 12 dd 0
+
+
+    its_usr1:
+        dq 1, 0                 ; it_interval: 1s, 0ns
+        dq 3, 0                 ; it_value   : 1s, 0ns (즉시 arm; 처음 만료 1초 뒤)
+
+
+    its_usr2:
+        dq 0, 100000000         ; it_interval: 0s, 100000000ns
+        dq 3, 0         ; it_value   : 0s, 100000000ns
+
+section .bss
+    timerid_usr1:   resq 1
+    timerid_usr2:   resq 1
+
+section .text
+
+usr1_handler:
+    inc qword [second_1_ticks]
+    PRNT sec_1
+    ret
+
+usr2_handler:
+    inc qword [second_0_1_ticks]
+    PRNT sec_0_1
+    ret
+
+restorer:
+    mov rax, SYS_rt_sigreturn
+    syscall
+
+
+install_second_alarm:
+    mov     rax, SYS_rt_sigaction
+    mov     rdi, SIGUSR1
+    lea     rsi, [sa_usr1]
+    xor     rdx, rdx                     ; oldact = NULL
+    mov     r10, 8                       ; sizeof(sigset_t)
+    syscall
+
+    test rax, rax
+    js  .sigusr1_fail 
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, txt1
+    mov rdx, txt1_len
+    syscall
+    ret
+
+.sigusr1_fail:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, failed1
+    mov rdx, failed1_len
+    syscall
+
+    ret
+
+
+install_0_1_second_alarm:
+    mov     rax, SYS_rt_sigaction
+    mov     rdi, SIGUSR2
+    lea     rsi, [sa_usr2]
+    xor     rdx, rdx                     ; oldact = NULL
+    mov     r10, 8                       ; sizeof(sigset_t)
+    syscall
+
+    test rax, rax
+    js  .sigusr1_fail 
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, txt2
+    mov rdx, txt2_len
+    syscall
+    ret
+
+.sigusr1_fail:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, failed2
+    mov rdx, failed2_len
+    syscall
+    ret
+
+
+global set_timers
+set_timers:
+    call install_second_alarm
+    call install_0_1_second_alarm
+
+    ; timer_create(CLOCK_MONOTONIC, &sevp_usr1, &timerid_usr1)
+    mov rax, SYS_timer_create
+    mov rdi, CLOCK_MONOTONIC
+    lea rsi, [sevp_usr1]
+    lea rdx, [timerid_usr1]
+    syscall
+
+    ; timer_settime(timerid_usr1, 0, &its_usr1, NULL)
+    mov rax, SYS_timer_settime
+    mov rdi, qword [timerid_usr1]  ; timer_t value
+    xor rsi, rsi                  ; flags = 0 (relative)
+    lea rdx, [its_usr1]
+    xor r10, r10                  ; old = NULL
+    syscall
+
+
+
+    ; timer_create(..., SIGUSR2, ...)
+    mov rax, SYS_timer_create
+    mov rdi, CLOCK_MONOTONIC
+    lea rsi, [sevp_usr2]
+    lea rdx, [timerid_usr2]
+    syscall
+
+    ; timer_settime(timerid_usr2, 0, &its_usr2, NULL)
+    mov rax, SYS_timer_settime
+    mov rdi, qword [timerid_usr2]
+    xor rsi, rsi
+    lea rdx, [its_usr2]
+    xor r10, r10
+    syscall
+
+    ret
